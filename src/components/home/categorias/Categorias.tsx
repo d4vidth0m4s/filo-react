bb622eb4a5e02aaed95d5efaeaaee616df1963e5
leidermadafakazz <tajo.musica@gmail.com>
2026-02-08 09:08:24 -0500

    Color categorias corregido

--- src/components/home/categorias/Categorias.tsx
+++ src/components/home/categorias/Categorias.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
+
 import './categorias.css'
 
 const Categorias = () => {
 
 const categorias = [
-  { nombre: 'Restaurantes', icono: 'https://png.pngtree.com/png-clipart/20250111/original/pngtree-pan-of-chinese-fried-rice-on-transparent-background-png-image_19048184.png' },
-  { nombre: 'Tiendas', icono: 'https://variedadesjired.com/wp-content/uploads/2022/03/variedades-jired-hero.webp' },
-  { nombre: 'Supermercados', icono: 'https://png.pngtree.com/png-vector/20241108/ourmid/pngtree-supermarket-shopping-cart-with-fresh-groceries-png-image_14304601.png' },
-  { nombre: 'Comidas Rápidas', icono: 'https://png.pngtree.com/png-vector/20240829/ourmid/pngtree-delicious-and-testy-cheese-burger-png-image_13659847.png' },
+  { nombre: 'Restaurantes', icono: '../public/images/restaurantes.png' },
+  { nombre: 'Tiendas', icono: '../public/images/tiendas.webp' },
+  { nombre: 'Supermercados', icono: '../public/images/supermercados.png' },
+  { nombre: 'Comidas Rápidas', icono: '../public/images/comidas-rapidas.png' },
   
 ]
 
